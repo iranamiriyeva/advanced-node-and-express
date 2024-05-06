7fd4c7c5cc305c2fdb1779ca8cdc878276fbd0a1
Irana Miriyeva <irana.miriyeva@gmail.com>
2024-05-06 21:12:46 +0000

    Solution3: Set up Passport

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+'use strict'
+require('dotenv').config()
+const express = require('express')
+const myDB = require('./connection')
+const fccTesting = require('./freeCodeCamp/fcctesting.js')
+//Solution3: Set up Passport
+const session = require('express-session')
+const passport = require('passport')
+const app = express()
 
-const app = express();
+//Solution1: Setup a Template Engine
+app.set('view engine', 'pug')
+app.set('views', './views/pug')
 
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+fccTesting(app) //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.route('/').get((req, res) => {
+//Solution2: Use a Template Engine's Powers
+  res.render('index', { title: 'Hello', message: 'Please log in' })
+})
 
-});
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
-});
+  console.log('Listening on port ' + PORT)
+})
